d4fce32db574fdd11e338f82bd221b770bfaa1f0
Forrest Parker <forrestwparker@gmail.com>
2018-05-30 12:54:19 -0700

    Completed Section 2: UIStackView by example

--- Multibrowser/ViewController.swift
+++ Multibrowser/ViewController.swift
@@ -7,16 +7,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
